3bee0a370207d7dabe699ab3d2984b8450992590
Shanghai Autobot <support@shanghai.techonology>
2022-08-25 11:01:26 -0400

    add apie_3xhPZnPsX38dJZTR5HHuX2.ts

---
+++ apie_3xhPZnPsX38dJZTR5HHuX2.ts
@@ -0,0 +1,6 @@
+import { getFormBodyFromJSON, invokeAPIEndpoint } from './lib'
+  
+export const callUntitledAPIEndpoint = async (publishableKey: string, params: { name : string }) => {
+  const form = getFormBodyFromJSON(params);
+  return invokeAPIEndpoint<string>("8616-68-199-153-76.ngrok.io", publishableKey, "apie_3xhPZnPsX38dJZTR5HHuX2", form)
+}
